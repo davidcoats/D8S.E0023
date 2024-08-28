9d787038b8638f8c0a890c627c3bc5ce573908ea
davidcoats <david@getsafetycone.com>
2024-08-27 22:51:01 -0700

    Interim commit.

---
+++ source/D8S.E0023/Code/_Types/_Classes/Class01.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace D8S.E0023
+{
+    /// <summary>
+    /// An example class.
+    /// </summary>
+    public class Class01
+    {
+        /// <summary>
+        /// A <see cref="string"/>-typed value for the class.
+        /// </summary>
+        public string Value { get; set; }
+
+
+        /// <summary>
+        /// Returns the value of the class.
+        /// </summary>
+        public string Get_Value()
+            => this.Value;
+    }
+}
--- source/D8S.E0023/Project Plan.md
+++ source/D8S.E0023/Project Plan.md
@@ -1,2 +1,2 @@
 # D8S.E0023
-An experimental project specifically for experiments with NuGet packaging.
+An experimental project specifically for experiments with the NuGet packaging process.
